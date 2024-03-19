7a2636917fc7c5efb315e090b2e82a103f8c9d1c
napatsawan-az <napatsawan.az@gmail.com>
2024-03-19 16:52:07 +0700

    event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import Status from "./components/Status";
-import Heading from "./components/Heading";
-import Oscar from "./components/Oscar";
-import Greet from "./components/Greet";
+import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
   return (
     <>
-      <Status status="success" />
-      <Heading>Placeholder text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Leonado Dicpario!</Heading>
-      </Oscar>
-      <Greet name="Vishwas" isLoggedIn={false} />
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button clicked");
+          console.log("event", event);
+          console.log("id", id);
+        }}
+      />
+      <Input
+        value=""
+        handleChange={(event) => {
+          console.log("event", event);
+        }}
+      />
     </>
   );
 }
