289088317101e8e9d0594ab4538d36fa225c85e1
napatsawan-az <napatsawan.az@gmail.com>
2024-03-23 16:20:18 +0700

    useContext Future Value

---
+++ src/components/context/User.tsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
+
+const User = () => {
+  const userContext = useContext(UserContext);
+
+  const handleLogin = () => {
+    userContext.setUser({
+      name: "Vishwas",
+      email: "vishwas@exemple.com",
+    });
+  };
+
+  const handleLogout = () => {
+    userContext.setUser(null);
+  };
+
+  return (
+    <>
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogout}>Logout</button>
+      <div>User name is {userContext.user?.name}</div>
+      <div>User email is {userContext.user?.email}</div>
+    </>
+  );
+};
+
+export default User;
